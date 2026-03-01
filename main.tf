70c16ddd1195fcb9b0bcaccb76ee311ddf7c1812
DjoPerp <djon0007@algonquinlive.com>
2026-02-28 23:27:26 -0500

    Create server (VM)

--- main.tf
+++ main.tf
@@ -123,3 +123,41 @@ data "cloudinit_config" "config" {
     content      = file("${path.module}/init.sh")
   }
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("C:\Users\perpe\Documents\AC\CST8918 300 - DevOps_Infrastructure as Code\Lab 5") 
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  user_data = data.cloudinit_config.config.rendered
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.pip.ip_address
+}
